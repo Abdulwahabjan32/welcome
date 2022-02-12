c358fc5cd74ce92bde858d8203bd2192b305fd79
Your Name <abdulwahabjan326@gmail.com>
2022-02-12 12:44:41 +0500

    Js file added

---
+++ java.js
@@ -0,0 +1,71 @@
+//Declaring a variable and assigning value to it
+var a=10;
+var b=15;
+alert("The value of a is =" + a +" and " + " Value of b is ="+ b)
+//Data types of primitive//
+//1.undefined:
+var a= ;
+console.log(a)
+//2.null:
+var a=null;
+console.log(a)
+//3.Number:
+var a=15;
+var b=10.25;
+console.log(a)
+//4.string:
+var a="I love Pakistan";
+console.log(a)
+//5.boolean:
+var a=true;
+var b=false;
+console.log(b)
+
+//DATA typws of Reference:
+//Array or object:
+var arr=[10,20,30,40]
+console.log(arr)
+// ( or by index) 
+console.log(arr[2]) 
+
+// Operrators in js
+//1.Arithimatic op:
+var j=15;
+var k=10;
+console.log("Adding J+k =",  j+k)
+console.log("Sub J-k =",  j-k)
+console.log("Multi J*k =",  j*k)
+console.log("Div J/k =",  j/k)
+
+//2.Assignment ops:
+var z=k;
+consolelog(c+=5)
+consolelog(c-=5)
+consolelog(c*=5)
+consolelog(c/=5)
+
+//3.Comparison ops:
+var a=10;
+var b=15;
+console.log(a>=b)
+console.log(a<=b)
+console.log(a>b)
+console.log(a<b)
+console.log(a==b)
+
+//4.logical ops
+var a=true;
+var b=false;
+//logical and:
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+//Logical or:
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
+//logical not:
+console.log(!true)
+console.log(!false)
